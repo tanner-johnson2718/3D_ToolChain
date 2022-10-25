593d0718c283cf4358c99ade8800338e23593330
Tanner <tanner.johnson2718@gmail.com>
2022-10-24 22:00:18 -0800

    ..

--- meme_ctlr/gui_test.py
+++ meme_ctlr/gui_test.py
@@ -1,27 +1,72 @@
 import PySimpleGUI as sg
+import serial
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Multiline('', key="printer_output", size=(80,20), disabled=1)],
+###############################################################################
+# Globals
+###############################################################################
+
+port_dev = "/dev/ttyUSB0"
+baud = 115200
+serial_timeout = 0
+port = 0                  # forward declaration of serial port obj
+
+###############################################################################
+# Set UP GUI
+###############################################################################
+
+# Define Window layout and Theme
+sg.theme('DarkAmber')
+layout = [  [sg.Multiline('', key="console", size=(80,20), disabled=1)],
             [sg.InputText(key="cmd_box", size=(80,1))],
         ]
 
-# Create the Window
+# Create the Window and define elements
 window = sg.Window('Window Title', layout, finalize=1)
 window["cmd_box"].bind("<Return>", "enter_hit")
-
-# Window Elements
-printer_output = window["printer_output"]
+console = window["console"]
 cmd_box = window["cmd_box"]
 
-# Event Loop to process "events" and get the "values" of the inputs
+# Helper to append multilin
+def multi_line_append(multi_line_obj, text):
+    multi_line_obj.update(multi_line_obj.get() + "\n" + text)
+    multi_line_obj.set_vscroll_position(1.0)
+
+###############################################################################
+# Set Serial port and helpers
+###############################################################################
+
+# Send a command to printer
+def send(port, cmd):
+    return port.write((cmd+"\n").encode('ascii'))
+
+# recv a single line from the printer
+def recv(port):
+    return port.readline().decode('ascii')
+
+port = serial.Serial(port = port_dev, baudrate = baud, timeout = serial_timeout)
+
+###############################################################################
+# Main Loop
+###############################################################################
+
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    event = ""
+    event, values = window.read(0)    # Check for GUI event
+    serial_input = recv(port)         # Check for serial input
+
+    # Window closed
+    if event == sg.WIN_CLOSED:
         break
+
+    # Enter hit while in command box
     elif event == "cmd_box" + "enter_hit":
-        printer_output.update(printer_output.get() + "\n" + cmd_box.get())
-        printer_output.set_vscroll_position(1.0)
+        command = cmd_box.get()
+        multi_line_append(console, "Sending) " + command)
         cmd_box.update("")
+        send(port, command)
+
+    # Serial input ready to be displayed
+    if serial_input != "" :
+        multi_line_append(console, serial_input)
 
 window.close()
