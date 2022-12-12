d626e50e6478c3eb6b874de834150cec24369bab
Tanner <tanner.johnson2718@gmail.com>
2022-12-11 19:47:07 -0900

    ..

--- Designs/hexagon_tile.py
+++ Designs/hexagon_tile.py
@@ -27,14 +27,11 @@ class Hexagon:
 
         self.line_numbers = []
         self.coincident_constraints = []
-        self.len_constraints = []
-        self.horizontal_contraints = []
-        self.angle_contraints = []
         self.pos_contraints = []
 
     def define_horizontally(self, side_len, left_point_x, left_point_y):
         self.p1.x = left_point_x
-        self.p2.y = left_point_y
+        self.p1.y = left_point_y
         self.length = side_len
 
         delta_x = side_len * math.cos(math.radians(60))
@@ -72,9 +69,11 @@ class Hexagon:
             c_no = sketch_object.addConstraint(Sketcher.Constraint('Coincident', l1,2,l2,1))
             self.coincident_constraints.append(c_no)
 
-        def constrain_line_len(l):
-            c_no = sketch_object.addConstraint(Sketcher.Constraint('Distance',l,self.length))
-            self.len_constraints.append(c_no)
+        def contrain_pos(l,x,y):
+            c_no = sketch_object.addConstraint(Sketcher.Constraint('DistanceX',-1,1,l,1,x))
+            self.pos_contraints.append(c_no)
+            c_no = sketch_object.addConstraint(Sketcher.Constraint('DistanceY',-1,1,l,1,y))
+            self.pos_contraints.append(c_no)   
         
         draw_line(self.p1,self.p2)
         draw_line(self.p2,self.p3) 
@@ -90,22 +89,48 @@ class Hexagon:
         connect_line(self.line_numbers[4], self.line_numbers[5])
         connect_line(self.line_numbers[5], self.line_numbers[0])
 
-        constrain_line_len(self.line_numbers[0])
-        constrain_line_len(self.line_numbers[1])
-        constrain_line_len(self.line_numbers[2])
-        constrain_line_len(self.line_numbers[3])
-        constrain_line_len(self.line_numbers[4])
-        constrain_line_len(self.line_numbers[5])
-
-        self.horizontal_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('Horizontal',self.line_numbers[1])))
-        self.horizontal_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('Horizontal',self.line_numbers[4])))
-
-        self.angle_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('Angle',self.line_numbers[0],2,self.line_numbers[1],1,math.radians(120))))
-        self.angle_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('Angle',self.line_numbers[5],2,self.line_numbers[0],1,math.radians(120))))
-
-        self.pos_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('DistanceX',-1,1,self.line_numbers[0],1,self.p1.x)))
-        self.pos_contraints.append(sketch_object.addConstraint(Sketcher.Constraint('DistanceY',-1,1,self.line_numbers[0],1,self.p1.y)))         
+        contrain_pos(self.line_numbers[0], self.p1.x, self.p1.y)
+        contrain_pos(self.line_numbers[1], self.p2.x, self.p2.y)
+        contrain_pos(self.line_numbers[2], self.p3.x, self.p3.y)
+        contrain_pos(self.line_numbers[3], self.p4.x, self.p4.y)
+        contrain_pos(self.line_numbers[4], self.p5.x, self.p5.y)
+        contrain_pos(self.line_numbers[5], self.p6.x, self.p6.y)       
         
+class HexGrid:
+    def __init__(self, side, start_x, start_y, n_row, n_col, brim_width):
+        self.side = side
+        self.start_x = start_x
+        self.start_y = start_y
+        self.n_row = n_row
+        self.n_col = n_col
+        self.brim_width = brim_width
+
+        self.y_delta = self.side * math.sin(math.radians(60))
+        self.x_delta = self.side * math.cos(math.radians(60))
+        
+        self.hexs = [0] * n_row
+        for i in range(0,n_row):
+            self.hexs[i] = [0] * n_col
+
+    def compute(self):
+        for i in range(0, self.n_row):
+            for j in range(0, self.n_col):
+                self.hexs[i][j] = Hexagon()
+
+                grid_x = self.start_x + j*(self.side + self.x_delta + self.brim_width)
+                grid_y = self.start_y - i*(2 * self.y_delta + self.brim_width)
+                
+                if j%2 == 0:
+                    # even
+                    self.hexs[i][j].define_horizontally(self.side, grid_x, grid_y)
+                else:
+                    # odd
+                    self.hexs[i][j].define_horizontally(self.side, grid_x, grid_y - self.y_delta - (self.brim_width/2))
+
+    def draw(self, sketch_object):
+        for i in range(0, self.n_row):
+            for j in range(0, self.n_col):
+                self.hexs[i][j].draw(sketch_object)
 
 # Main macro code
 def hexagon():
@@ -125,14 +150,9 @@ def hexagon():
 
     print("Found Sketch")
 
-    hex = Hexagon()
-    hex.define_horizontally(10,0,0)
-    hex.draw(sketch)
-
-    hex2 = Hexagon()
-    hex2.define_horizontally(10,20,0)
-    hex2.draw(sketch)
-        
+    hex_grid = HexGrid(side=10, start_x=-30, start_y=50, n_row=4, n_col=5, brim_width=5)
+    hex_grid.compute()
+    hex_grid.draw(sketch)
         
             
 hexagon()
