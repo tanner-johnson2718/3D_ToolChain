deb79621af2bef34647193aa5b6e10cafc66ca8f
Tanner <tanner.johnson2718@gmail.com>
2022-12-21 00:19:01 -0900

    ..

--- meme_ctlr/meme_backend.py
+++ meme_ctlr/meme_backend.py
@@ -1,9 +1,8 @@
-import sender
-import recver
 import data_store
 import threading
 import time
 import serial
+import os
 
 port_dev = "/dev/ttyACM0"
 baud = 115200
@@ -13,41 +12,56 @@ killed = 0
 ds = data_store.DataStore(0, 0)
 port = serial.Serial(port = port_dev, baudrate = baud, timeout = serial_timeout)
 
-def recv_thread(self):
-    print("Recver Thread Started")
-    while not self.killed:
-            serial_input = port.readline().decode('ascii')
-            if serial_input != "":
-                self.ds.push_reponse_line(serial_input)
+pipe_name = "./pipe"
+os.mkfifo(pipe_name, 0o600)
+os.system("gnome-terminal -e 'bash -c \"cat " + pipe_name + "\"'")
 
-def send_thread(self):
-        print("Sender Thread Starting")
-        while not self.killed:
-            cmd = self.ds.wait_on_next_to_send()
-            if (not self.killed) and isinstance(cmd, str):
-                port.write((cmd+"\n").encode('ascii'))
-        print("Sender Thread Killed")
+def recv_thread():
+    print("Recv Thread Starting...")
+    while not killed:
+        serial_input = port.readline().decode('ascii')
+        if (not killed) and (serial_input != ""):
+            ds.push_reponse_line(serial_input)
+    print("Recv Thread Stopping...")
+
+def send_thread():
+    print("Send Thread Starting...")
+    while not killed:
+        cmd = ds.wait_on_next_to_send()
+        if (not killed) and isinstance(cmd, str):
+            port.write((cmd+"\n").encode('ascii'))
+    print("Send Thread Stopping...")
+
+def response_poster():
+    with open(pipe_name, "w") as fifo:
+        print("Response Thread Starting...")
+        while not killed:
+            r = ds.wait_on_next_response()
+            fifo.write(r)
+            fifo.flush()
+    print("Response Thread Stopping...")
 
 
+send_t = threading.Thread(target=send_thread, name="Send_Thread")
+send_t.start()
 
+recv_t = threading.Thread(target=recv_thread, name="Recv_Thread")
+recv_t.start()
 
-send_thread = threading.Thread(target=s.thread, name="Send_Thread")
-send_thread.start()
+response_t = threading.Thread(target=response_poster, name="Respnse Thred")
+response_t.start()
 
-recv_thread = threading.Thread(target=r.thread, name="Recv_Thread")
-recv_thread.start()
-
-
-
-time.sleep(10)
+while not killed:
+    t = input("cmd) ")
+    if t == "q":
+        killed = 1
+        break
+    else:
+        ds.push_next_send(t)
 
 ds.kill()
-s.kill()
-r.kill()
-
-
-send_thread.join()
-recv_thread.join()
-
+send_t.join()
+response_t.join()
+recv_t.join()
 port.close()
-
+os.system("rm -rf __pycache__ " + pipe_name)
