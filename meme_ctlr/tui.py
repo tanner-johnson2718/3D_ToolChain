4b3d7c1f07c21690e50b4120dce42130c3ef17a1
Tanner <tanner.johnson2718@gmail.com>
2022-12-24 17:11:04 -0900

    ...

--- meme_ctlr/tui.py
+++ meme_ctlr/tui.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import TextLog, Header, Footer, Static
+from textual.widgets import TextLog, Header, Footer, Static, Input
 from textual import events
 from textual.containers import Container
 
@@ -14,9 +14,14 @@ class MEME(App):
         yield Footer()
         yield TextLog(id="t1", classes="box")
         yield TextLog(id="t2", classes="box")
+        yield Input(id="i1", classes="box")
 
     def on_key(self, event: events.Key) -> None:
         self.query_one("#t1",TextLog).write(event.character)
+
+    def on_input_submitted(self, event : Input.Submitted):
+        text = self.query_one("#i1").value
+        self.query_one("#t2").write(text)
 
 
 if __name__ == "__main__":
