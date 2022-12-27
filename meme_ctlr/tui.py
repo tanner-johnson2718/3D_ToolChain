6dd2024f949934d04fa55e2eac0948025bb783f1
Tanner <tanner.johnson2718@gmail.com>
2022-12-27 13:23:47 -0900

    ..

--- meme_ctlr/tui.py
+++ meme_ctlr/tui.py
@@ -19,19 +19,32 @@ class MEME(App):
         yield TextLog(id="t1", classes="box")
         yield TextLog(id="t2", classes="box")
         yield Input(id="i1", classes="box")
-
         
         asyncio.create_task(self.recv_thread())
 
-    def on_input_submitted(self, event : Input.Submitted):
+    async def on_input_submitted(self, event : Input.Submitted):
         text = self.query_one("#i1").value
-        self.query_one("#t2").write(text)
+        self.query_one("#t2").write("Sending -> " + text)
+        self.writer.write((text+"\n").encode('ascii'))
 
     async def recv_thread(self):
         self.reader, self.writer = await asyncio.open_connection(HOST, PORT)
+        buffer = ""
         while True:
             data = await self.reader.read(PACKET_SIZE)
-            self.query_one("#t2").write(data.decode('ascii'))
+            buffer += data.decode('ascii')
+
+            while '\n' in buffer:
+                index = buffer.find('\n')
+                prefix = buffer[0:4]
+                buffer_temp = buffer[5:index]
+                target = "#t2"
+                if prefix == "subR":
+                    target = "#t2"
+                else:
+                    target = "#t1"
+                self.query_one(target).write(buffer_temp)
+                buffer = buffer[(index+1):]
 
 
 app = MEME()
